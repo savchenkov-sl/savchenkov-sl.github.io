23aa3b22c50d3bd49a9d7fc02f0f1c37d1152400
savchenkov-sl <80706129+savchenkov-sl@users.noreply.github.com>
2021-03-23 23:23:49 +0300

    0.0.4

---
+++ js/6.f008f1a3.js
@@ -0,0 +1,1 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([[6],{"71ee":function(a,e,t){"use strict";t.r(e);var n=t("7a23");const s=Object(n["Q"])("data-v-5b1f4f2f"),d=s(((a,e,t,n,s,d)=>" Задачи "));var f={name:"Tasks",data(){return{}},methods:{}};f.render=d,f.__scopeId="data-v-5b1f4f2f";e["default"]=f}}]);
